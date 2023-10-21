30f396c417a08bf140e480af556ff724f6717485
Tushar Chandra <64960756+me-tusharchandra@users.noreply.github.com>
2023-10-21 22:01:07 +0530

    a2 q1

---
+++ Assignment-2/Q1.py
@@ -0,0 +1,114 @@
+# Written by: Tushar Chandra - 2021211
+
+# Importing necessary libraries
+import os
+import cv2
+import numpy as np
+
+# Output directory
+output_dir = 'Output'
+os.makedirs(output_dir, exist_ok=True)
+
+# Loading the image
+img = cv2.imread('barbara.bmp', cv2.IMREAD_GRAYSCALE)
+
+# Salt and Pepper Noise function
+def snp(img, noise):
+    noisyImage = img.copy()
+    totalPixels = img.size
+    numSalt = int(totalPixels * noise)
+    numPepper = int(totalPixels * noise)
+
+    # Adding salt noise
+    saltCoords = [np.random.randint(0, i - 1, numSalt) for i in img.shape]
+    noisyImage[saltCoords[0], saltCoords[1]] = 255
+
+    # Adding pepper noise
+    pepperCoords = [np.random.randint(0, i - 1, numPepper) for i in img.shape]
+    noisyImage[pepperCoords[0], pepperCoords[1]] = 0
+
+    return noisyImage
+
+# Median filter function
+def median(img, winSize):
+    length, breadth = img.shape
+    res = np.zeros((length, breadth), dtype=np.uint8)
+
+    # Calculate the half-window size
+    halfWindow = winSize // 2
+
+    # Iterate through each pixel in the image
+    for i in range(length):
+        for j in range(breadth):
+            window = []
+
+            # Iterate over a neighborhood around the current pixel
+            for m in range(-halfWindow, halfWindow + 1):
+                for n in range(-halfWindow, halfWindow + 1):
+
+                    # Check if the pixel coordinates are within the image boundaries
+                    if 0 <= i + m < length and 0 <= j + n < breadth:
+                        window.append(img[i + m, j + n])
+            # Calculate the median of the pixel values in the window
+            res[i, j] = np.median(window)
+
+    return res
+
+# Function to compute PSNR
+def PSNR(cleanImg, denoisedImg):
+    mse = np.mean((cleanImg - denoisedImg) ** 2)
+    if mse == 0:
+        return float('inf')
+
+    maxIntensity = 255
+
+    psnr = 10 * np.log10((maxIntensity ** 2) / mse)
+    return psnr
+
+# Noise levels
+noiseLevels = [0.05, 0.15, 0.20, 0.25]
+
+# Window sizes
+windowSizes = [3, 5, 7]
+
+bestParams = {} 
+bestPSNR = {} 
+
+print()
+
+# Looping over all noise levels
+for noiseLevel in noiseLevels:
+    print("--------------------")
+    print(f"Noise Level: {noiseLevel * 100}%")
+    noisyImage = snp(img, noiseLevel)
+    cleanImage = img
+    bestPSNR[noiseLevel] = -1 
+
+    # Looping over all window sizes
+    for winSize in windowSizes:
+        denoisedImage = median(noisyImage, winSize)
+        psnr_value = PSNR(cleanImage, denoisedImage)
+
+        if psnr_value > bestPSNR[noiseLevel]:
+            bestPSNR[noiseLevel] = psnr_value
+            bestParams[noiseLevel] = winSize
+
+        # Save the noisy and denoised images in the Output folder
+        noisy_image_path = os.path.join(output_dir, f'noisy_image_{int(noiseLevel * 100)}_{winSize}x{winSize}.png')
+        denoised_image_path = os.path.join(output_dir, f'denoised_image_{int(noiseLevel * 100)}_{winSize}x{winSize}.png')
+
+        # Save the noisy and denoised images, overwriting if they already exist
+        cv2.imwrite(noisy_image_path, noisyImage, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+        cv2.imwrite(denoised_image_path, denoisedImage, [cv2.IMWRITE_PNG_COMPRESSION, 0])
+
+        print(f"Window Size: {winSize}x{winSize}, PSNR: {psnr_value:.2f}")
+
+# Printing the best parameters
+print()
+print("--------------------")
+print("Best Parameters:")
+for noiseLevel, winSize in bestParams.items():
+    print(f"Best denoising parameter for {int(noiseLevel * 100)}% noise: {winSize}x{winSize} (PSNR: {bestPSNR[noiseLevel]:.2f})")
+
+print()
+print("--------------------")
